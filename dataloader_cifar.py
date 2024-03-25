e22c42f7a099b1d569d9b405a258732dad31aa66
Penalty_kl <34411318+yuanpengtu@users.noreply.github.com>
2024-03-25 10:58:37 +0800

    Update dataloader_cifar.py

--- dataloader_cifar.py
+++ dataloader_cifar.py
@@ -56,7 +56,7 @@ class cifar_dataset(Dataset):
            transforms.RandomHorizontalFlip(),
            CIFAR10Policy(),
            transforms.ToTensor(),
-           transforms.Normalize((0.4914, 0.4822, 0.4465),(0.2023, 0.1994, 0.2010))])         
+           transforms.Normalize(mean=mean, std=std)])         
         self.transform_strong = transforms.Compose([
             transforms.RandomCrop(32, padding=4),
             transforms.RandomHorizontalFlip(),
